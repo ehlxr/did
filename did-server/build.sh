e6af00845c16a41799e62668e699d38f34006c72
ehlxr <ehlxr.me@gmail.com>
2018-08-15 11:02:38 +0800

    delelte ignore file

--- did-server/build.sh
+++ did-server/build.sh
@@ -3,12 +3,10 @@
 echo "############## start ##############"
 BUILD_DATE=`date +%Y-%m-%d:%H:%M:%S`
 
-cd ../
 SERVER_NAME=`awk '/<name>[^<]+<\/name>/{gsub(/<name>|<\/name>/,"",$1);print $1;exit;}' pom.xml`
 #SERVER_NAME=$(basename `pwd`)
 mvn clean install -DskipTests
-cp target/${SERVER_NAME}*.jar ./docker
-cd docker
+cp target/${SERVER_NAME}*.jar .
 
 err=$?
 echo "############## $err ##############"
@@ -17,13 +15,9 @@ if [ "$err" -ne "0" ]; then
 	exit 1
 fi
 
-base_url=10.19.248.200:30100
-docker_url=${base_url}/ceres/${SERVER_NAME}:latest
+docker_url=ehlxr/${SERVER_NAME}:latest
 
 docker build --build-arg SERVER_NAME=${SERVER_NAME} -f ./Dockerfile -t ${docker_url} .
-
-#echo "docker login -u ggov  ${base_url}"
-docker login -u ggov -p !qaz2wsX ${base_url}
 
 docker push ${docker_url}
 
