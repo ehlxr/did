00f9e3efab529de96cc0c43897d10616d1d2df97
ehlxr <ehlxr.me@gmail.com>
2018-08-15 10:40:37 +0800

    update testcase

--- did-sdk/src/test/java/cn/ceres/did/DidSdkPressTest.java
+++ did-sdk/src/test/java/cn/ceres/did/DidSdkPressTest.java
@@ -4,6 +4,8 @@ import cn.ceres.did.client.InvokeCallback;
 import cn.ceres.did.client.ResponseFuture;
 import cn.ceres.did.client.SdkClient;
 import cn.ceres.did.sdk.SdkProto;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,17 +17,26 @@ import java.util.concurrent.CountDownLatch;
  *
  * @author ehlxr
  */
-public class DidSdkPressAsyncTest {
-    private static final Logger logger = LoggerFactory.getLogger(DidSdkPressAsyncTest.class);
-    // 初始发送总量
-    private static int NUM = 80000;
+public class DidSdkPressTest {
+    private static final Logger logger = LoggerFactory.getLogger(DidSdkPressTest.class);
+    private SdkClient client;
 
-    @Test
-    public void pressAsyncTest() throws Exception {
-        SdkClient client = new SdkClient("10.19.248.200",30581);
+    @Before
+    public void init() {
+        client = new SdkClient("10.19.248.200",30581);
+        // client = new SdkClient();
         client.init();
         client.start();
+    }
 
+    @After
+    public void destroy() {
+        client.shutdown();
+    }
+
+
+    @Test
+    public void asyncTest() throws Exception {
         long start;
         long end;
         long cast;
@@ -33,6 +44,8 @@ public class DidSdkPressAsyncTest {
         long allcast = 0;
 
         for (int k = 0; k < 10; k++) {
+            // 初始发送总量
+            int NUM = 80000;
             final CountDownLatch countDownLatch = new CountDownLatch(NUM);
             start = System.currentTimeMillis();
             for (int i = 0; i < NUM; i++) {
@@ -58,5 +71,35 @@ public class DidSdkPressAsyncTest {
         }
 
         logger.info("invokeAsync test all num is: {}, all cast time: {} millsec, all throughput: {} send/millsec", amount, allcast, (double) amount / allcast);
+    }
+
+    @Test
+    public void syncTest() throws Exception {
+        long start;
+        long end;
+        long cast;
+        long amount = 0;
+        long allcast = 0;
+
+        for (int k = 0; k < 20; k++) {
+            start = System.currentTimeMillis();
+            int NUM = 60000;
+            for (int i = 0; i < NUM; i++) {
+                final SdkProto sdkProto = new SdkProto();
+                client.invokeSync(sdkProto, 5000);
+            }
+
+            end = System.currentTimeMillis();
+            cast = (end - start);
+            allcast += cast;
+
+            logger.info("invokeSync test num is: {}, cast time: {} millsec, throughput: {} send/millsec", NUM, cast, (double) NUM / cast);
+
+            amount += NUM;
+            // NUM += 5000;
+            // TimeUnit.SECONDS.sleep(2);
+        }
+
+        logger.info("invokeSync test all num is: {}, all cast time: {} millsec, all throughput: {} send/millsec", amount, allcast, (double) amount / allcast);
     }
 }
